323d27f620ee928db3192269a93c38a00e21d808
Jeremiah Ramiscal <jereberries1215@gmail.com>
2023-06-12 09:11:29 -0700

    started styling header

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import "./Title.css";
+const Title = () => {
+
+    return(
+        <div className = "title">
+            <h2>Grocery List</h2>
+            <button>Add</button>
+        </div>
+    );
+};
+
+export default Title;
