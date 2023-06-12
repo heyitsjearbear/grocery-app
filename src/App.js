9dbc3d3dd94175c8219baa2a4e772236ca42b27d
Jeremiah Ramiscal <jereberries1215@gmail.com>
2023-06-12 13:03:58 -0700

    added todo list in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Title from "./components/Title";
 import List from "./components/List";
 const App = () => {
+  //TODO child-to-parent pass form submission from Title component all the way up so you can append
+  // to this items array, and append back to List component dynamically
   const items = [
     { food: "banana", amount: 2 },
     { food: "apple", amount: 4 },
@@ -8,7 +10,7 @@ const App = () => {
   return (
     <div>
       <Title />
-      <List itemList = {items} />
+      <List itemList={items} />
     </div>
   );
 };
