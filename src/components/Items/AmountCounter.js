8a69aff90c4521de62fa594a974de6e23569e1dc
Jeremiah Ramiscal <jereberries1215@gmail.com>
2023-06-20 10:36:45 -0700

    made amount counter not go passed 0

--- src/components/Items/AmountCounter.js
+++ src/components/Items/AmountCounter.js
@@ -1,22 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 const AmountCounter = (props) => {
   const [newAmount, setNewAmount] = useState(props.amount);
-  const plusOneItem = () =>{
-    setNewAmount((prevAmount) =>{
-      return prevAmount+1;
+  const plusOneItem = () => {
+    setNewAmount((prevAmount) => {
+      if (prevAmount === 0) return 0;
+      return prevAmount + 1;
     });
   };
-  const minusOneItem = () =>{
-    setNewAmount((prevAmount) =>{
-      return prevAmount-1;
+  const minusOneItem = () => {
+    setNewAmount((prevAmount) => {
+      if (prevAmount === 0) return 0;
+      return prevAmount - 1;
     });
   };
   return (
-    <div className = "counter">
+    <div className="counter">
       <h2>{newAmount}</h2>
       <div className="button-container">
-        <button onClick = {plusOneItem} className="plus-button">+</button>
-        <button onClick = {minusOneItem} className="minus-button">-</button>
+        <button onClick={plusOneItem} className="plus-button">
+          +
+        </button>
+        <button onClick={minusOneItem} className="minus-button">
+          -
+        </button>
       </div>
     </div>
   );
