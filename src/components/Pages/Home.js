bfef9d35551f53f9e771837218294801e913289d
heyitsjearbear <jereberries1215@gmail.com>
2023-06-25 16:36:50 -0700

    finished based on tutorial now debug

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import NewItemPanel from "../NewItemPanel";
 import List from "../List";
+import { useLocation, useNavigate } from "react-router-dom";
 const Home = () => {
+  const location = useLocation();
   return (
     <div>
+      <h1>Hello {location.state.id} and welcome to the home</h1>
       <NewItemPanel onNewItemAdded={saveNewItemHandler} />
       <List itemList={items} />
     </div>
